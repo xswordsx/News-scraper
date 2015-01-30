c38a73bfddb660449fa8e5cf644ee9e431166e69
Ivan Latunov <ivan.latunov@chaosgroup.com>
2015-01-30 13:40:58 +0200

    Initial commit for looper

---
+++ Looper/index.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+
+var Notifer = require('../Notifier/index');
+var Subscriber = require('../Subscriber/index');
+var Scraper = require('../Scraper/index');
